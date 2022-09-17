b25118e6a2f0725ce507ee1e57381769ff9983f5
Artem <kypidon71@gmail.com>
2022-09-17 20:51:12 +0300

    addZad1

---
+++ Zad1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Write number 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write number 2");
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (a < b) max = b; 
+   
+Console.WriteLine($"MAX number = {max}");
