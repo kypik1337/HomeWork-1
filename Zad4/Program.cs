37fac676c030386af911b654793cc6edae220032
Artem <kypidon71@gmail.com>
2022-09-17 21:53:29 +0300

    Переделанная задача номер 4

--- Zad4/Program.cs
+++ Zad4/Program.cs
@@ -1,17 +1,17 @@
 ﻿Console.WriteLine("Write number;#");
 int a = int.Parse(Console.ReadLine());
- while (a >= 0) 
-    {
-        if (a % 2 ==0)
+int b = 0;
+ while (b < a) 
+     {
+        if ( a % 2 == 0)
         {
-        Console.WriteLine(a+ "");
-        a = a - 2;
+       b = b + 2;
+       Console.Write($"{b} , ");
         }
-         if (a % 2 !=0)
+        if ( a % 2 != 0)
         {
-            a = a -1;
-        Console.WriteLine(a+ "");
-        a = a - 2;
+            a = a - 1;
+            b = b + 2;
+            Console.Write($"{b} , ");
         }
-    }
- 
+     }
