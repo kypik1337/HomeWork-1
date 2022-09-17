0705efad83b302cc5e1a5d74f5ab2c0855d5599c
Artem <kypidon71@gmail.com>
2022-09-17 21:11:48 +0300

    addZad3

---
+++ Zad3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write number;3");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+    {
+        Console.WriteLine("VIN Chetnoe");
+    }
+    if (a % 2 != 0)
+    {
+        Console.WriteLine("defeat ne Chetnoe");
+    }
