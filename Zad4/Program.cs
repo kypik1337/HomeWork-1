6fcf7a338c02381835174ff3b5be52387c049de0
Artem <kypidon71@gmail.com>
2022-09-17 21:39:06 +0300

    VINzad4

---
+++ Zad4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Write number;#");
+int a = int.Parse(Console.ReadLine());
+ while (a >= 0) 
+    {
+        if (a % 2 ==0)
+        {
+        Console.WriteLine(a+ "");
+        a = a - 2;
+        }
+         if (a % 2 !=0)
+        {
+            a = a -1;
+        Console.WriteLine(a+ "");
+        a = a - 2;
+        }
+    }
+ 
